10765d7882ffd0d13ed3ae9037cfd1b90a5aa75e
Vas11a <104201639+Vas11a@users.noreply.github.com>
2022-06-04 13:23:40 +0300

    bug-fixed

--- scripts/index.php
+++ scripts/index.php
@@ -7,16 +7,15 @@ $time = $_POST['time'];
 $age = $_POST['age'];
 $message = $_POST['message'];
 
-header('location: https://vas11a.github.io/Spa-web-site/');
 
-// $send =  "<h2>All Datas</h2> Name: <div>$name</div> <br> Email: <div>$email</div> <br> Number: <div>$number</div> <br> Data: <div>$data</div> <br> Time: <div>$time</div> <br> Age: <div>$age</div> <br> MEssage: <div>$message</div>";
+$send =  "<h2>All Datas</h2> Name: <div>$name</div> <br> Email: <div>$email</div> <br> Number: <div>$number</div> <br> Data: <div>$data</div> <br> Time: <div>$time</div> <br> Age: <div>$age</div> <br> MEssage: <div>$message</div>";
 
-// $to = "vasapanov721@gmail.com";
-// $from = $email;
+$to = "vasapanov721@gmail.com";
+$from = $email;
 
-// $headers = "From: $from\r\nReply-to: $from\r\nContent-type: html/plain; charset=utf-8\r\n";
+$headers = "From: $from\r\nReply-to: $from\r\nContent-type: html/plain; charset=utf-8\r\n";
 
-// mail($to, $send, $send, $headers);
+mail($to, $send, $send, $headers);
 
 //unfortunately function mail() does not work on github pages
 
