c3b5848f1469d5ba805ef5d0ff9122c1e4f9fd68
Vasia Panov <vasapanov721@gmail.com>
2022-06-04 13:04:19 +0300

    add php file

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 
+$('.nav').click(function (e) {
+	$('.nav').removeClass('add-clas')
+	$('.burg1').removeClass('add-clas2')
+	$('.burg2').removeClass('add-clas3')
+})
 
-$('.img-menu , .nav ').click(function(e) {
+$('.img-menu').click(function(e) {
 	$('.nav').toggleClass('add-clas')
 	$('.burg1').toggleClass('add-clas2')
 	$('.burg2').toggleClass('add-clas3')
-});
-
+})
 
 $('.button-send').click(function() {
 		let name = $('.input-name').val();
@@ -15,12 +19,12 @@ $('.button-send').click(function() {
 		let age = $('.input-age').val();
 		let number = $('.input-number').val();
 	if (name == '' || data == ''  || email == '' || time == '' || age == '' || number == '') {
+		$('.button-send').attr("type" , "button");
 		alert('Please fill in al fields')
-	} else {
 		$('.forma')[0].reset();
-		setTimeout(function(){
-			alert('Success!')
-		}, 1500)
+	} else {
+		$('.button-send').attr("type", "submit");
+		alert('Success!')
 	}
 });
 
