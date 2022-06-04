c62ebe4c62dbe80a396c38827bce9f21765eb0b9
Vas11a <104201639+Vas11a@users.noreply.github.com>
2022-06-04 13:10:02 +0300

    Update index.php

--- scripts/index.php
+++ scripts/index.php
@@ -9,14 +9,14 @@ $message = $_POST['message'];
 
 // header('location: /index.html');
 
-$send =  "<h2>All Datas</h2> Name: <div>$name</div> <br> Email: <div>$email</div> <br> Number: <div>$number</div> <br> Data: <div>$data</div> <br> Time: <div>$time</div> <br> Age: <div>$age</div> <br> MEssage: <div>$message</div>";
+// $send =  "<h2>All Datas</h2> Name: <div>$name</div> <br> Email: <div>$email</div> <br> Number: <div>$number</div> <br> Data: <div>$data</div> <br> Time: <div>$time</div> <br> Age: <div>$age</div> <br> MEssage: <div>$message</div>";
 
-$to = "vasapanov721@gmail.com";
-$from = $email;
+// $to = "vasapanov721@gmail.com";
+// $from = $email;
 
-$headers = "From: $from\r\nReply-to: $from\r\nContent-type: html/plain; charset=utf-8\r\n";
+// $headers = "From: $from\r\nReply-to: $from\r\nContent-type: html/plain; charset=utf-8\r\n";
 
-mail($to, $send, $send, $headers);
+// mail($to, $send, $send, $headers);
 
 //unfortunately function mail() does not work on github pages
 
